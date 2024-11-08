be4775750df659c312f0ecdb1b7a35750bfb0c87
iker0411 <145666360+iker0411@users.noreply.github.com>
2024-11-08 13:51:42 +0100

    metodo multiplicar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Main {
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
