8010af362af5fcd21c9b6422fc7c910148ac765f
JLandero19 <118732003+JLandero19@users.noreply.github.com>
2024-11-08 13:56:07 +0100

    Método dividir

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 
 public class Main {
+    public static float dividir(int a, int b) {
+        return (float) a / b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
